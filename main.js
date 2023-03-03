97e8c6973f153695ba6abed1666ebb208d886a8a
VenomExE <121004003+ExE-Venom@users.noreply.github.com>
2023-03-03 10:03:05 +0530

    2.0 CHANGES

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const Insta = require('./insta.js');
-const client = new Insta.Client();
-const chatbot = require("node-fetch").default;
+const Insta = require('./insta.js'); //importing INSTA.JS package
+const client = new Insta.Client(); //installing client
+const chatbot = require("node-fetch").default; //importing node-fetch to fetch replies from api
 
+// LOG MESSAGE ONCE BOT IS ONLINE
 client.on('connected', () => {
     console.log(`${client.user.username} Is Ready Now For Chats`);
 });
 
+// HANDELING MESSAGES
 client.on('messageCreate', (message) => {
     if (message.author.id === client.user.id) return
     message.markSeen();
 
-    if(message.content.toLowerCase().includes('hi')){ 
-        return message.chat.sendMessage('VENOM IS MY DEVELOPER CHECK OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
+    if(message.content.toLowerCase().includes('hi') || message.content.toLowerCase().includes('hello')){ 
+        return message.chat.sendMessage('VENOMEXE IS MY DEVELOPER CHECK OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
     } else
-    chatbot(`https://brv-chat.vercel.app/api?message=${encodeURIComponent(message.content)}`)
+    chatbot(`http://api.brainshop.ai/get?bid=${process.env.API_ID}&key=${process.env.API_KEY}&uid=[uid]&msg=${encodeURIComponent(message.content)}`)
     .then(res => res.json())
     .then(json => {
-      message.chat.sendMessage(json.reply);
+      message.chat.sendMessage(json.cnt);
     }).catch(err => {});
 });
 
-client.login('<insta-username>', '<insta-password>');
+// BOT LOGIN
+client.login(process.env.USERNAME, process.env.PASSWORD);
